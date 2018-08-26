1cf36e1b834a059e29eaa75cd7d41aadc1778093
王炳尧 <wangbingyao@lawn-143-215-118-140.lawn.gatech.edu>
2018-08-26 17:51:38 -0400

    Initial Commit

---
+++ MyApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyApp
+//
+//  Created by 王炳尧 on 2018/8/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
